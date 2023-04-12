817c9b936be7fd838cd8655271c967b0141cca5c
Ryan Vermeulen <RyanTheDev@outlook.com>
2023-04-12 12:34:47 +0200

    AlarmReceiver.kt will now take the alarm ID intent extra for printing via the debug log call

--- app/src/main/java/com/triangle/ryansalarm/AlarmReceiver.kt
+++ app/src/main/java/com/triangle/ryansalarm/AlarmReceiver.kt
@@ -3,13 +3,16 @@ package com.triangle.ryansalarm
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 
 class AlarmReceiver : BroadcastReceiver()
 {
 	override fun onReceive(context: Context?, intent: Intent?)
 	{
 		val message = intent?.getStringExtra("EXTRA_MESSAGE") ?: return
+		val id = intent.getIntExtra("ALARM_ID", -1)
 
-		println("Alarm triggered: $message")
+		Log.d( "AlarmReceiver","Alarm $id triggered: $message")
+		MediaSingleton.play()
 	}
 }
