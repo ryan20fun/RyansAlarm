90fa6d21b1efc759f1737471b9d3ea3c3829eda0
Ryan Vermeulen <RyanTheDev@outlook.com>
2023-04-13 17:26:10 +0200

    [MediaSingleton] +Added code to gradually increase the volume at a fixed rate +Rearranged and added some documentation comments

--- app/src/main/java/com/triangle/ryansalarm/MediaSingleton.kt
+++ app/src/main/java/com/triangle/ryansalarm/MediaSingleton.kt
@@ -6,14 +6,40 @@ import android.net.Uri
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
+import androidx.core.math.MathUtils
+import java.util.*
+import kotlin.concurrent.fixedRateTimer
 
 object MediaSingleton
 {
+	//region Public Properties
+	/**
+	 * Controls how long to wait before automatically stopping the audio playback
+	 */
 	var AutoStopDelayMS: Long = 5000
 
-	private var player: MediaPlayer? = null
-	private var handler: Handler? = null
+	/**
+	 * The initial volume to start playing at
+	 */
+	var InitialVolume: Double = 0.05
 
+	/**
+	 * The maximum volume to use
+	 */
+	var MaxVolume: Double = 1.0
+
+	/**
+	 * The volume increase per step
+	 */
+	var VolumeStep: Double = 0.05
+	//endregion
+
+	//region Public method(s)
+	/**
+	 * Initialises the singleton with the specified audio uri for the media player
+	 * @param context The context to use for the media player instance
+	 * @param SoundURI The uri for the audio file to use
+	 */
 	fun initialise(context: Context, SoundURI: Uri)
 	{
 		//region Release resources if needed
@@ -42,19 +68,45 @@ object MediaSingleton
 
 	fun play()
 	{
+		this.player!!.setVolume(this.InitialVolume.toFloat(), this.InitialVolume.toFloat())
 		this.player!!.seekTo(0)
 		this.player!!.start()
 
 		this.handler?.postDelayed(delayStop(), AutoStopDelayMS)
+		this.volumeTimer = fixedRateTimer(name = "volume", period = 500, action = { setVolume() })
 	}
+
+	fun stop()
+	{
+		this.volumeTimer?.cancel()
+		this.player?.stop()
+	}
+	//endregion
+
+	//region Protected Method(s)
 
 	private fun delayStop(): () -> Unit = {
 		this.stop()
 		Log.d("MediaSingleton", "play post delayed called")
 	}
 
-	fun stop()
+	private fun setVolume()
 	{
-		this.player?.stop()
+		this.CurrentVolume = MathUtils.clamp(
+			this.CurrentVolume + this.VolumeStep,
+			this.InitialVolume, this.MaxVolume
+		)
+
+		val volume = this.CurrentVolume.toFloat()
+		this.player?.setVolume(volume, volume)
 	}
+	//endregion
+
+	//region Protected Member(s)
+	private var player: MediaPlayer? = null
+	private var handler: Handler? = null
+
+	private var CurrentVolume: Double = 0.05
+	private var volumeTimer: Timer? = null
+	//endregion
 }
